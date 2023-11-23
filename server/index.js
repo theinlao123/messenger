2b852258ae8e10c01f0d558968a2e1fa3aeef74e
Artem <jhxdetrg@gmail.com>
2023-11-23 23:25:01 +0500

    create server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+// Подключаем библиотеку Express
+const express = require('express');
+// Создаем объект приложения Express
+const app = express();
+// Устанавливаем порт, на котором будет работать сервер
+const PORT = 5000;
+// Подключаем библиотеку http и создаем сервер, используя объект приложения Express
+const http = require('http').Server(app);
+// Подключаем библиотеку cors для обработки кросс-доменных запросов
+const cors = require('cors');
+// Подключаем библиотеку socket.io и передаем ей созданный сервер http
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: 'http://localhost:5173'
+    }
+})
+
+
+app.get('api', (req, res) => {
+    res.json({
+        message: 'hello'
+    })
+});
+
+http.listen(PORT, () => {
+    console.log('Server working')
+})
